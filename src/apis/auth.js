5285a1e41d79efe146ef21e37b85a128b56b4429
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-11 08:03:24 +0700

    update code

---
+++ src/apis/auth.js
@@ -0,0 +1,16 @@
+import BaseApi from ".";
+
+const login = async (email, password) => {
+	const response = await BaseApi.post("/login", {
+		email: email,
+		password: password,
+	});
+
+	return response.data;
+};
+
+const AuthApi = {
+	login,
+};
+
+export default AuthApi;

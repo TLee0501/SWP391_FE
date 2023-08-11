90bbb95039687de75c5128306ef36173afb87b03
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-11 10:15:22 +0700

    update course ui

--- src/__mocks__/course.js
+++ src/__mocks__/course.js
@@ -3,18 +3,18 @@ export const mockCourses = [
 		id: "1",
 		code: "SWP391",
 		name: "Software Development Projects",
-		description: "Software Development Projects",
+		active: true,
 	},
 	{
 		id: "2",
 		code: "PRO101",
 		name: "Fundamental Programming with C",
-		description: "Fundamental Programming with C",
+		active: true,
 	},
 	{
 		id: "3",
 		code: "SWP491",
 		name: "Capstone Projects",
-		description: "Capstone Projects",
+		active: false,
 	},
 ];
--- src/pages/course/list/components/CourseItem.js
+++ src/pages/course/list/components/CourseItem.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Button, Card, Col, Row, Typography } from "antd";
+import { Button, Card, Col, Row, Tag, Typography } from "antd";
 import { Down, Up } from "@icon-park/react";
 
-const { Text, Paragraph } = Typography;
+const { Text } = Typography;
 
 export const CourseItem = ({ course, onDelete, onUpdate }) => {
 	const [expanded, setExpanded] = useState(false);
@@ -19,23 +19,18 @@ export const CourseItem = ({ course, onDelete, onUpdate }) => {
 
 	const renderExpandedInfo = () => {
 		return (
-			<div>
-				<Paragraph className="mt-2 mb-0 pb-0">
-					<strong>Mô tả:</strong> {course.description}
-				</Paragraph>
-				<Row gutter={8}>
-					<Col>
-						<Button type="primary" onClick={handleUpdate}>
-							Cập nhật
-						</Button>
-					</Col>
-					<Col>
-						<Button danger onClick={handleDelete}>
-							Xóa
-						</Button>
-					</Col>
-				</Row>
-			</div>
+			<Row gutter={8} className="mt-2">
+				<Col>
+					<Button type="primary" onClick={handleUpdate}>
+						Cập nhật
+					</Button>
+				</Col>
+				<Col>
+					<Button danger onClick={handleDelete}>
+						Xóa
+					</Button>
+				</Col>
+			</Row>
 		);
 	};
 
@@ -44,11 +39,19 @@ export const CourseItem = ({ course, onDelete, onUpdate }) => {
 	};
 
 	return (
-		<Card className="w-full cursor-pointer" onClick={toggleExpanded}>
+		<Card className="w-full cursor-pointer" hoverable onClick={toggleExpanded}>
 			<Row justify="space-between">
-				<Text className="text-lg font-medium">
-					[{course.code}] {course.name}
-				</Text>
+				<div>
+					<Text className="text-lg font-medium" ellipsis>
+						[{course.code}] {course.name}
+					</Text>
+					<Tag
+						className="ml-2"
+						color={course.active ? "blue-inverse" : "default"}
+					>
+						{course.active ? "Kích hoạt" : "Chưa kích hoạt"}
+					</Tag>
+				</div>
 				{!expanded ? <Down size={26} /> : <Up size={26} />}
 			</Row>
 			{expanded && renderExpandedInfo()}

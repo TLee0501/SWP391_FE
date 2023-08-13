dff74b7a5add5b35bb0657169e416709d0ae12ab
Duy Nguyen <khanhduyx2@gmail.com>
2023-08-13 10:16:28 +0700

    Handle check authentication

--- src/router/RootRoute.js
+++ src/router/RootRoute.js
@@ -1,6 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
+import routes from "../constants/routes";
 
 const RootRoute = () => {
+  const navigate = useNavigate();
+  const isAuthenticated = useAuth();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate(routes.dashboard.root);
+    } else {
+      navigate(routes.login);
+    }
+  }, []);
+
   return <Outlet />;
 };
 

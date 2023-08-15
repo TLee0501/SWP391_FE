41c2d78627fd507dd226e1523f7b7482eeca0bd6
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-15 08:06:36 +0700

    update code

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,5 +1,5 @@
 import { Layout, Typography } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AppSider } from "./Sider";
 import { AppHeader } from "./Header";
 import { Outlet, useLocation } from "react-router-dom";
@@ -13,51 +13,50 @@ const { Content } = Layout;
 const { Title } = Typography;
 
 export const Dashboard = () => {
+	const location = useLocation();
+	const [user, setUser] = useState();
 
-const location = useLocation();
-const [user, setUser] = useState()
-	
-  useEffect(() => {
-    AuthApi.getUser().then((user) => setUser(user));
-  }, []);
+	useEffect(() => {
+		AuthApi.getUser().then((user) => setUser(user));
+	}, []);
 
 	return (
-     <UserContext.Provider value={{user: user, setUser: setUser}}>
-		<Layout hasSider>
-			<AppSider />
-			<Layout
-				style={{
-					marginLeft: 200,
-				}}
-			>
-				<AppHeader />
-				<Content
+		<UserContext.Provider value={{ user: user, setUser: setUser }}>
+			<Layout hasSider>
+				<AppSider />
+				<Layout
 					style={{
-						padding: "16px 16px 0",
-						overflow: "initial",
-						backgroundColor: "white",
+						marginLeft: 200,
 					}}
 				>
-					{location.pathname === routes.dashboard.root && (
-						<div className="flex-center" style={{ flexDirection: "column" }}>
-							<Lottie
-								width="30%"
-								options={{
-									animationData: animationData,
-									autoplay: true,
-									loop: true,
-									rendererSettings: {
-										preserveAspectRatio: "xMidYMid slice",
-									},
-								}}
-							/>
-							<Title level={3}>SWP Projects On-going Report System</Title>
-						</div>
-					)}
-					<Outlet />
-				</Content>
+					<AppHeader />
+					<Content
+						style={{
+							padding: "16px 16px 0",
+							overflow: "initial",
+							backgroundColor: "white",
+						}}
+					>
+						{location.pathname === routes.dashboard.root && (
+							<div className="flex-center" style={{ flexDirection: "column" }}>
+								<Lottie
+									width="30%"
+									options={{
+										animationData: animationData,
+										autoplay: true,
+										loop: true,
+										rendererSettings: {
+											preserveAspectRatio: "xMidYMid slice",
+										},
+									}}
+								/>
+								<Title level={3}>SWP Projects On-going Report System</Title>
+							</div>
+						)}
+						<Outlet />
+					</Content>
+				</Layout>
 			</Layout>
-		</Layout>
-</UserContext.Provider>
+		</UserContext.Provider>
 	);
 };
--- src/pages/error/404.js
+++ src/pages/error/404.js
@@ -1,5 +1,5 @@
 const PageNotFound = () => {
-  return <h1>Page Not Found - 404</h1>;
+	return <div></div>;
 };
 
 export default PageNotFound;

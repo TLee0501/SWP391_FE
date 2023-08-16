66778f64f7c17ae22b0fba4c959661948aef44c5
kenly <hungppmse140153@fpt.edu.vn>
2023-08-16 07:57:17 +0700

    update

--- src/pages/class/detail/components/StudentList.js
+++ src/pages/class/detail/components/StudentList.js
@@ -2,43 +2,46 @@ import { Table, Tag, Tooltip } from "antd";
 import React from "react";
 
 export const StudentList = ({ students }) => {
-	const columns = [
-		{
-			title: "Họ tên",
-			dataIndex: "fullName",
-			key: "fullName",
-		},
-		{
-			title: "Email",
-			dataIndex: "email",
-			key: "email",
-		},
-		{
-			title: "Nhóm đề tài",
-			dataIndex: "team",
-			key: "team",
-			render: (_, { team }) => {
-				return (
-					<Tooltip
-						title={
-							team ? team.members.map((e) => e.name).join(", ") : undefined
-						}
-					>
-						<Tag color={team ? "blue-inverse" : "default"}>
-							{team ? team.name : "Chưa có"}
-						</Tag>
-					</Tooltip>
-				);
-			},
-		},
-	];
+  const columns = [
+    {
+      title: "Họ tên",
+      dataIndex: "fullName",
+      key: "fullName",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Trạng thái nhóm",
+      dataIndex: "team",
+      key: "team",
+      render: (_, { team }) => {
+        return (
+          <Tooltip
+            title={
+              team ? team.members.map((e) => e.name).join(", ") : undefined
+            }
+          >
+            <Tag
+              STYLE={"font-size:14px"}
+              color={team ? "blue-inverse" : "default"}
+            >
+              {team ? team.name : "Chưa có"}
+            </Tag>
+          </Tooltip>
+        );
+      },
+    },
+  ];
 
-	return (
-		<Table
-			pagination={false}
-			bordered
-			dataSource={students}
-			columns={columns}
-		/>
-	);
+  return (
+    <Table
+      pagination={false}
+      bordered
+      dataSource={students}
+      columns={columns}
+    />
+  );
 };
--- src/pages/project/list/index.js
+++ src/pages/project/list/index.js
@@ -1,7 +1,53 @@
-import React from "react";
+import { Plus } from "@icon-park/react";
+import { Button, Input, Row } from "antd";
+import React, { useState } from "react";
+import { ProjectModal } from "../components/ProjectModal";
+import AccountList from "../../account/list/components/AccountList";
 
 const ProjectListPage = () => {
-  return <div>ProjectListPage</div>;
+  const [showCreateProjectModal, setShowCreateProjectModal] = useState(false);
+	const [showUpdateProjectModal, setShowUpdateProjectModal] = useState(false);
+
+	const handleShowCreateProjectModal = () => {
+		setShowCreateProjectModal(true);
+	};
+	const handleCloseCreateProjectModal = () => {
+		setShowCreateProjectModal(false);
+	};
+
+	const handleShowUpdateProjectModal = () => {
+		setShowUpdateProjectModal(true);
+	};
+	const handleCloseUpdateProjectModal = () => {
+		setShowUpdateProjectModal(false);
+	};
+
+	return (
+		<div>
+			<Row justify="space-between mb-2">
+				<Input.Search placeholder="Tìm dự án..." className="w-1/2" />
+				<Button
+					onClick={handleShowCreateProjectModal}
+					type="primary"
+					className="flex-center"
+					icon={<Plus />}
+				>
+					Thêm dự án
+				</Button>
+			</Row>
+			<AccountList onEditAccount={handleShowUpdateProjectModal} />
+			<ProjectModal
+				title="Thêm dự án"
+				open={showCreateProjectModal}
+				onCancel={handleCloseCreateProjectModal}
+			/>
+			<ProjectModal
+				title="Cập nhật dự án"
+				open={showUpdateProjectModal}
+				onCancel={handleCloseUpdateProjectModal}
+			/>
+		</div>
+	);
 };
 
 export default ProjectListPage;

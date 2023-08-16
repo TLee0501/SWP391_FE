78e1343173b09d603835b3b054b010cf043efb8f
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-16 09:12:11 +0700

    add project api

--- src/pages/project/detail/index.js
+++ src/pages/project/detail/index.js
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { mockProjects } from "../../../apis/project";
 import { BasePageContent } from "../../../layouts/containers/BasePageContent";
 import { Card, Typography } from "antd";
+import { mockProjects } from "../../../__mocks__/project";
 
 const { Paragraph } = Typography;
 
 const ProjectDetailPage = () => {
-  const { id } = useParams();
+	const { id } = useParams();
 
-  const [project, setProject] = useState({});
+	const [project, setProject] = useState({});
 
-  useEffect(() => {
-    const data = mockProjects.find((e) => e.id === id);
-    if (data) {
-      setProject(data);
-    }
-  }, [id]);
+	useEffect(() => {
+		const data = mockProjects.find((e) => e.id === id);
+		if (data) {
+			setProject(data);
+		}
+	}, [id]);
 
-  return (
-    <BasePageContent title={project?.name}>
-      <Card title="Mô tả dự án" className="mt-4">
-        <Paragraph>{project.description}</Paragraph>
-      </Card>
-    </BasePageContent>
-  );
+	return (
+		<BasePageContent title={project?.name}>
+			<Card title="Mô tả dự án" className="mt-4">
+				<Paragraph>{project.description}</Paragraph>
+			</Card>
+		</BasePageContent>
+	);
 };
 
 export default ProjectDetailPage;

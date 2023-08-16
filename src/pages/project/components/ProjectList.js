eed0d3469a47d8c5f8af7477611891c5b21b95ed
kenly <hungppmse140153@fpt.edu.vn>
2023-08-16 07:56:58 +0700

    update

--- src/pages/project/components/ProjectList.js
+++ src/pages/project/components/ProjectList.js
@@ -4,7 +4,7 @@ import { mockAccounts } from "../../../../__mocks__/account";
 import { roles } from "../../../../constants/app";
 import { Edit } from "@icon-park/react";
 
-const AccountList = ({ onEditAccount }) => {
+const ProjectList = ({ onEditAccount }) => {
 	const getRoleName = (role) => {
 		switch (role) {
 			case roles.ADMIN:
@@ -23,13 +23,11 @@ const AccountList = ({ onEditAccount }) => {
 			title: "Email",
 			dataIndex: "email",
 			key: "email",
-			sorter: (a, b) => a.email.localeCompare(b.email),
 		},
 		{
 			title: "Họ tên",
 			dataIndex: "fullName",
 			key: "fullName",
-			sorter: (a, b) => a.fullName.localeCompare(b.fullName),
 		},
 		{
 			title: "Vai trò",
@@ -50,7 +48,6 @@ const AccountList = ({ onEditAccount }) => {
 					</Tag>
 				);
 			},
-			sorter: (a, b) => a.role - b.role,
 		},
 		{
 			title: "Trạng thái",
@@ -63,7 +60,6 @@ const AccountList = ({ onEditAccount }) => {
 					</Tag>
 				);
 			},
-			sorter: (a, b) => a.status - b.status,
 		},
 		{
 			title: "Thao tác",
@@ -84,4 +80,4 @@ const AccountList = ({ onEditAccount }) => {
 	return <Table dataSource={mockAccounts} columns={columns} />;
 };
 
-export default AccountList;
+export default ProjectList;

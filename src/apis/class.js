e0a66f2bc592c8986e7fe47b480e2b9d5c669781
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-17 07:11:43 +0700

    handle get classes

--- src/apis/class.js
+++ src/apis/class.js
@@ -12,8 +12,47 @@ const createClass = async (data) => {
 	}
 };
 
+const searchClass = async (courseId, keyword) => {
+	try {
+		var queries = [];
+		if (courseId) {
+			queries.push(`courseId=${courseId}`);
+		}
+		if (keyword) {
+			queries.push(`searchText=${keyword}`);
+		}
+
+		const query = queries.length > 0 ? `?${queries.join("&")}` : "";
+
+		const response = await BaseApi.get(`/${resource}/SearchClass${query}`);
+		return response.data;
+	} catch (error) {
+		console.log("Error search class: ", error);
+		return [];
+	}
+};
+
+const getAllClasses = async (courseId) => {
+	try {
+		var queries = [];
+		if (courseId) {
+			queries.push(`courseId=${courseId}`);
+		}
+
+		const query = queries.length > 0 ? `?${queries.join("&")}` : "";
+
+		const response = await BaseApi.get(`/${resource}/GetAllClasses${query}`);
+		return response.data;
+	} catch (error) {
+		console.log("Error get all classes: ", error);
+		return [];
+	}
+};
+
 const ClassApi = {
 	createClass,
+	searchClass,
+	getAllClasses,
 };
 
 export default ClassApi;

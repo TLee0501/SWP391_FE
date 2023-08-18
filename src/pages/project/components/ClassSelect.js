3a25639adb6f559bd522b5d342f087da4707e0b8
Duy Nguyen <duykhanhnguyen@kms-technology.com>
2023-08-18 15:58:59 +0700

    update code

--- src/pages/project/components/ClassSelect.js
+++ src/pages/project/components/ClassSelect.js
@@ -23,6 +23,7 @@ export const ClassSelect = ({
 
 	useEffect(() => {
 		getClasses();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	const classOptions = classes.map((e) => {
